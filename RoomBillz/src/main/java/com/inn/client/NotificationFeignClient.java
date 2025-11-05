e840d8dee2d36f122032fa2a9cd13e564f778bb7
sarfaraz alam <sarfarazalam2702@gmail.com>
2025-11-05 23:23:01 +0530

    roombillz - Day-25

--- RoomBillz/src/main/java/com/inn/client/NotificationFeignClient.java
+++ RoomBillz/src/main/java/com/inn/client/NotificationFeignClient.java
@@ -3,8 +3,7 @@ package com.inn.client;
 import org.springframework.cloud.openfeign.FeignClient;
 
 
-@FeignClient(name = "ROOMBILLZ-NOTIFICATION-SERVICE", url = "${notification.service.url}",
-             configuration = {FeignConfig.class,FeignRetryConfig.class})
+@FeignClient(name = "ROOMBILLZ-NOTIFICATION-SERVICE",configuration = {FeignConfig.class,FeignRetryConfig.class})
 public interface NotificationFeignClient {
 
 }
