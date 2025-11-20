57a03aa566f855218978c8ee1866b399ddfcfd17
sarfaraz alam <sarfarazalam2702@gmail.com>
2025-11-21 01:18:55 +0530

    roombillz - Day-33

--- RoomBillz/src/main/java/com/inn/client/IEventMessageNotificationClient.java
+++ RoomBillz/src/main/java/com/inn/client/IEventMessageNotificationClient.java
@@ -9,9 +9,9 @@ import org.springframework.web.bind.annotation.RequestBody;
 import com.inn.dto.EventMessageCreateRequestDto;
 import com.inn.dto.ResponseDto;
 
-@FeignClient(name = "ROOMBILLZ-NOTIFICATION-SERVICE", path = "/api/v1/notification/eventMessage",configuration = {FeignConfig.class,FeignRetryConfig.class})
+@FeignClient(name = "ROOMBILLZ-NOTIFICATION-SERVICE", url = "${notification.service.url}", path = "/eventMessage", configuration = { FeignConfig.class, FeignRetryConfig.class })
 public interface IEventMessageNotificationClient {
 
-	  @PostMapping(path = "/createEventMessage", consumes = {MediaType.APPLICATION_JSON_VALUE})
-	  public ResponseEntity<ResponseDto> createEvent(@RequestBody EventMessageCreateRequestDto eventMessageCreateRequestDto);
+	@PostMapping(path = "/createEventMessage", consumes = MediaType.APPLICATION_JSON_VALUE)
+	ResponseEntity<ResponseDto> createEvent(@RequestBody EventMessageCreateRequestDto eventMessageCreateRequestDto);
 }
